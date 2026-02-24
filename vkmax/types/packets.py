a8531c69b9bb04f73233ab2584c2a4c148e1656d
MarsBatya <mars.hall.tg.fck@gmail.com>
2026-02-24 21:50:08 +0500

    make payloads behave better

--- vkmax/types/packets.py
+++ vkmax/types/packets.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
-from typing import Any, Optional
+from typing import Any, Literal, Optional, Union
 
-from adaptix import Retort, NameStyle, name_mapping
+from adaptix import Chain, Retort, NameStyle, loader, name_mapping
 
 from vkmax.types.messages import (
     Message,
@@ -20,7 +20,7 @@ from vkmax.types.chats import Chat, ChatReactions
 @dataclass(slots=True)
 class Payload:
     message: Message
-    chat_id: Optional[int] = None
+    chat_id: int
     unread: Optional[int] = None
     mark: Optional[int] = None
     ttl: Optional[bool] = None
@@ -34,31 +34,7 @@ class Payload:
     # for pinned msgs looks like
     chat: Optional[Chat] = None
 
-@dataclass(slots=True)
-class DeletedPayload:
-    chat_id: int
-    message_ids: list[str]
-
-@dataclass(slots=True)
-class ChatPayload:
-    chat: Chat
-    message: Optional[Message] = None
-    chat_id: Optional[int] = None
-    mark: Optional[int] = None
-    prev_message_id: Optional[str] = None
-    ttl: Optional[bool] = None
-    unread: Optional[int] = None
-
-# ---------------- Packet ----------------
-
-
-@dataclass(slots=True)
-class Packet:
-    ver: int
-    cmd: int
-    seq: int
-    opcode: int
-    payload: Payload|DeletedPayload|ChatPayload
+    _flag: Literal["_NORMAL"] = "_NORMAL"
 
     @classmethod
     def from_dict(cls, data: dict):
@@ -71,13 +47,66 @@ class Packet:
     def part_to_dict(cls, anything: Any) -> dict:
         return _retort.dump(anything)
 
+@dataclass(slots=True)
+class DeletedPayload:
+    chat_id: int
+    message_ids: list[str]
+    _flag: Literal["_DELETED"] = "_DELETED"
+
+@dataclass(slots=True)
+class ChatPayload:
+    chat: Chat
+    message: Optional[Message] = None
+    chat_id: Optional[int] = None
+    mark: Optional[int] = None
+    prev_message_id: Optional[str] = None
+    ttl: Optional[bool] = None
+    unread: Optional[int] = None
+    _flag: Literal["_CHAT"] = "_CHAT"
+
+@dataclass(slots=True)
+class EditedMessagePayload:
+    message: Message
+    _flag: Literal["_EDITED"] = "_EDITED"
+
+# ---------------- Packet ----------------
+
+
+@dataclass(slots=True)
+class Packet:
+    ver: int
+    cmd: int
+    seq: int
+    opcode: int
+    payload: Union[Payload, DeletedPayload, ChatPayload, EditedMessagePayload]
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return _retort.load(data, cls)
+
+    def to_dict(self) -> dict:
+        return _retort.dump(self)
+
+    @classmethod
+    def part_to_dict(cls, anything: Any) -> dict:
+        return _retort.dump(anything)
+
+# more control over what to load
+def packet_loader(data: dict):
+    if "chatId" not in data["payload"] and "message" in data["payload"]:
+        data["payload"]["_flag"] = "_EDITED"
+    elif "messageIds" in data["payload"]:
+        data["payload"]["_flag"] = "_DELETED"
+    elif "chat" in data["payload"] and "message" not in data["payload"]:
+        data["payload"]["_flag"] = "_CHAT"
+    else:
+        data["payload"]["_flag"] = "_NORMAL"
+    return data
+
 _retort = Retort(
     recipe=[
         # Global snake_case <-> camelCase conversion
-        name_mapping(
-            Packet,
-            name_style=NameStyle.CAMEL,
-        ),
+        loader(Packet, packet_loader, chain=Chain.FIRST),
         *(
             name_mapping(
                 _type,
@@ -88,13 +117,14 @@ _retort = Retort(
                 Payload,
                 DeletedPayload,
                 ChatPayload,
+                EditedMessagePayload,
                 DelayedAttributes,
                 Chat,
                 ChatReactions,
                 ReactionInfo,
             )
         ),
-        * (
+        *(
             name_mapping(
                 _type,
                 name_style=NameStyle.CAMEL,
